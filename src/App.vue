06840e5ba64dabad8e0079724492f15d27969d79
Jaque <53920655+scallopc@users.noreply.github.com>
2024-08-28 19:09:23 -0300

    to-do (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,110 @@
 <template>
-  <ToDo v-bind:todos="todos" @del-todo="deleteTodo" />
+  <div class="container-main">
+    <Header />
+    <AddToDo v-on:add-todo="addTodo" />
+    <ToDo v-bind:todos="paginatedTodos" @del-todo="deleteTodo" />
+    <Paginator
+      :currentPage="currentPage"
+      :totalPages="totalPages"
+      @page-changed="changePage"
+    />
+  </div>
 </template>
 
-<script>
+<script scope>
 import ToDo from './components/toDo.vue';
+import Header from './components/header.vue';
+import AddToDo from './components/addToDo.vue';
+import axios from 'axios';
+import Paginator from './components/paginator.vue';
 
 export default {
   name: 'App',
   components: {
     ToDo,
+    Header,
+    AddToDo,
+    Paginator,
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: false,
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false,
-        },
-      ],
+      todos: [],
+      currentPage: 1,
+      todosPerPage: 10,
     };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.todos.length / this.todosPerPage);
+    },
+    paginatedTodos() {
+      const start = (this.currentPage - 1) * this.todosPerPage;
+      const end = start + this.todosPerPage;
+      return this.todos.slice(start, end);
+    },
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(
+          (res) => (this.todos = this.todos.filter((todo) => todo.id !== id))
+        )
+        .catch((err) => console.log(err));
     },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios
+        .post('https://jsonplaceholder.typicode.com/todos', {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+    changePage(page) {
+      this.currentPage = page; // Atualiza a página atual ao clicar no paginador
+    },
+  },
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => (this.todos = res.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Poppins, sans-serif;
+  font-size: 14px;
+  line-height: 1.4;
+}
+
+.container-main {
+  margin: auto;
+  max-width: 100%;
+  width: 640px;
+  box-shadow: 0 0 4px 0px #b5b1b145;
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+    &:hover {
+      background: #666;
+    }
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack'); // Importando o webpack
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true, // Defina como true se estiver usando a Options API
+        __VUE_PROD_DEVTOOLS__: false, // False para desabilitar devtools em produção
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // False para desabilitar logs detalhados de erro de mismatch em produção
+      }),
+    ],
+  },
+});
